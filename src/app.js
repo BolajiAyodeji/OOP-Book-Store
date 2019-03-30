e5f171b3a1979d82a23974b42dd675da2bf3a081
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-30 14:55:22 +0100

    revamp to es6 classes

--- src/app.js
+++ src/app.js
@@ -1,58 +1,51 @@
 
-// Book Constructor
-function Book(title, author, isbn) {
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
-}
-
-// UI Constructor
-function UI() {}
-
-// Add book to list
-UI.prototype.addBookToList = (book) => {
-   const list = document.getElementById('book-list');
-   const row = document.createElement('tr');
-
-   row.innerHTML = `
-   <td>${book.title}</td>
-   <td>${book.author}</td>
-   <td>${book.isbn}</td>
-   <td><a href="#" class="delete">&times;</a></td>
-   `;
-
-   list.appendChild(row);
-}
-
-// Show Alert
-UI.prototype.showAlert = (message, className) => {
-  const div = document.createElement('div');
-  div.className = `alert ${className}`;
-  const text = document.createTextNode(message);
-  div.appendChild(text);
-
-  const container = document.querySelector('.container');
-  const form = document.querySelector('#book-form');
-  container.insertBefore(div, form);
-
-  // error timeout
-  setTimeout(() => {
-    document.querySelector('.alert').remove();
-  }, 3000);
-}
-
-// delete book
-UI.prototype.deleteBook = (target) => {
-  if(target.className === 'delete') {
-    target.parentElement.parentElement.remove();
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
   }
 }
 
-// Clear Fields
-UI.prototype.clearFields = () => {
-  title.value = '';
-  author.value = '';
-  isbn.value = '';
+class UI {
+  addBookToList(book) {
+    const list = document.getElementById('book-list');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">&times;</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+  showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    const text = document.createTextNode(message);
+    div.appendChild(text);
+
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+    // error timeout
+    setTimeout(() => {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+  deleteBook(target) {
+    if(target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
+  }
+  clearFields() {
+    title.value = '';
+    author.value = '';
+    isbn.value = '';
+  }
 }
 
 // Event Listener for add book
